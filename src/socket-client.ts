3365f04bdd34328185beecbe5971088b54307b77
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-14 02:19:21 -0500

    Add listener server status

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,6 +1,17 @@
-import { Manager } from "socket.io-client"
+import { Manager, Socket } from "socket.io-client"
 
 export const connectToServer = () => {
-const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
-const socket = manager.socket('/');
+    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
+    const socket = manager.socket('/');
+    addListeners(socket);
+}
+
+const addListeners = (socket: Socket) => {
+    const serverStatusLabel = document.querySelector('#server-status');
+    socket.on('connect', () => {
+        serverStatusLabel!.innerHTML = 'online';
+    });
+    socket.on('disconnect', () => {
+        serverStatusLabel!.innerHTML = 'offline';
+    });
 }
