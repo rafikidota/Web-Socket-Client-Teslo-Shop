615023f066d6ff59ced5c46d41b530dc71c4b079
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-14 02:19:55 -0500

    Add server status and ul

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-   
     <h1>Web Socket - Client</h1>
-    <span>Offline</span>
+    <span id="server-status">connecting</span>
+    <ul id="client-ul">
+    <li>David</li>
+    </ul>
   </div>
 `;
 connectToServer();
