1cb131ca4ee07b9039c18925c14e325b22ac5bbf
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-12-17 18:18:46 -0500

    Emit message to server and auth

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,27 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Web Socket - Client</h1>
-    <span id="server-status">connecting</span>
-    <ul id="clients-ul">
-    <li>David</li>
-    </ul>
+    <input id="jwt" placeholder="json web token"/>
+    <button id="btn-connect">Connect</button>
+    <br/>
+    <span id="server-status">disconnected</span>
+    <ul id="clients-ul"></ul>
+    <form id="message-form">
+        <input placeholder="message" id="message-input" autocomplete="off">
+    </form>
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
   </div>
 `;
-connectToServer();
+// connectToServer();
+
+const jwt = document.querySelector<HTMLInputElement>('#jwt')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect');
+btnConnect!.addEventListener('click', () => {
+  if (jwt.value.trim().length <= 0) {
+    return alert('Enter a jwt');
+  }else{
+    connectToServer(jwt.value.trim());
+    jwt.innerText = '';
+  }
+});
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,20 +1,32 @@
 import { Manager, Socket } from "socket.io-client"
 
-export const connectToServer = () => {
-    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
+interface MessageFromServer {
+    fullName: string,
+    message: string
+}
+
+export const connectToServer = (jwt: string) => {
+    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+        extraHeaders: {
+            auth: jwt
+        }
+    });
     const socket = manager.socket('/');
     addListeners(socket);
 }
 
 const addListeners = (socket: Socket) => {
-    const serverStatusLabel = document.querySelector('#server-status');
-    const clientsUl = document.querySelector('#clients-ul');
+    const serverStatusLabel = document.querySelector<HTMLSpanElement>('#server-status');
+    const clientsUl = document.querySelector<HTMLUListElement>('#clients-ul');
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form');
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input');
+    const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul');
 
     socket.on('connect', () => {
-        serverStatusLabel!.innerHTML = 'online';
+        serverStatusLabel!.innerHTML = 'connected';
     });
     socket.on('disconnect', () => {
-        serverStatusLabel!.innerHTML = 'offline';
+        serverStatusLabel!.innerHTML = 'disconnected';
     });
     socket.on('clients-updated', (clients: string[]) => {
         let clientHtml = '';
@@ -24,6 +36,29 @@ const addListeners = (socket: Socket) => {
             `
         });
         clientsUl!.innerHTML = clientHtml;
+    });
+    messageForm!.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if (messageInput!.value.trim().length <= 0) {
+            return;
+        }
+        socket.emit('message-from-client', {
+            id: 'me!!',
+            message: messageInput!.value
+        });
+        messageInput!.value = '';
+    });
+
+    socket.on('message-from-server', (payload: MessageFromServer) => {
+        const newMessage = `
+            <li>
+                <strong>${payload.fullName}:</strong>
+                <span>${payload.message}</span>
+            </li>
+            `
+        const li = document.createElement('li');
+        li.innerHTML = newMessage;
+        messagesUl!.append(li);
     });
 
 }
